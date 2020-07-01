c9d538be4a06e256b57bf2c3a971d5e12d1da818
Glauber Kyves Oliveira <glauberkyves@gmail.com>
2020-07-01 16:27:26 -0300

    Update AdyenPayment.swift

--- ios/AdyenPayment.swift
+++ ios/AdyenPayment.swift
@@ -198,6 +198,29 @@ class AdyenPayment: RCTEventEmitter {
         self.showPayment(component,componentData : componentData,paymentDetails : paymentDetails)
     }
     
+    @objc func encryptCard(
+          _ cardInfo:NSDictionary,
+          resolver resolve: RCTPromiseResolveBlock,
+          rejecter reject: RCTPromiseRejectBlock
+        ) -> Void {
+            let card = CardEncryptor.Card(
+                number: cardInfo["number"] as? String,
+                securityCode: cardInfo["cvc"] as? String,
+                expiryMonth: cardInfo["expiryMonth"] as? String,
+                expiryYear: cardInfo["expiryYear"] as? String
+            )
+            
+            let publicKey = cardInfo["publicKey"] as! String;
+            
+            let encryptedCard = CardEncryptor.encryptedCard(for: card, publicKey: publicKey)
+            
+            resolve([
+                "number": encryptedCard.number,
+                "expiryYear": encryptedCard.expiryYear,
+                "expiryMonth": encryptedCard.expiryMonth,
+                "cvc": encryptedCard.securityCode
+            ])
+    }
     func showPayment(_ component: NSString,componentData : NSDictionary,paymentDetails : NSDictionary){
         DispatchQueue.main.async {
             let rootViewController = UIApplication.shared.delegate?.window??.rootViewController
